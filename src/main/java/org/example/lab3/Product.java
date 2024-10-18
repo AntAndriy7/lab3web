40d0aa2bd133d640a5dbb98890c77c91c8538b9f
ㅤАндрій <andriy.antonov7@gmail.com>
2024-10-18 14:10:08 +0300

    first commit

---
+++ src/main/java/org/example/lab3/Product.java
@@ -0,0 +1,11 @@
+package org.example.lab3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+}
